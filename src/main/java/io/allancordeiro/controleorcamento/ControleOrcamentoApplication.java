8ffa4279e8c215969d67191ffa0be9787ee03b3e
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2022-02-03 20:07:47 -0300

    docker-compose configurations

--- src/main/java/io/allancordeiro/controleorcamento/ControleOrcamentoApplication.java
+++ src/main/java/io/allancordeiro/controleorcamento/ControleOrcamentoApplication.java
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
 @SpringBootApplication
 public class ControleOrcamentoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ControleOrcamentoApplication.class, args);
 	}
